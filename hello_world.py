2ac22a94319307821d249210a5a8c64ed0ec6625
natdog00 <natdog00@gmail.com>
2022-05-10 16:44:42 -0700

    first commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+from operator import le
+
+
+welcome = ('Hello, welcome')
+print(welcome)
+
+Name = input("Please enter your comapny's name")
+print(Name)
+
+Cable = input("Please eneter the amount of cable in feet")
+print(Cable)
+X = int(Cable)
+
+Closing =('Thank you, ')
+print(Closing, end = '' )
+print(Name)
+print(X * .87)
